d0352d15885ebecf39a1a3695797ee73d109e43d
Sly-Perez <elaipereza@gmail.com>
2025-02-26 17:32:49 -0500

    handler folder added: ErrorHandler.php file added (We will handle the exceptions of our project through a custom class ErrorHandler with public static functions that allow this)

---
+++ src/handler/ErrorHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+class ErrorHandler{
+
+    public static function handleException(Throwable $e): void{
+        http_response_code(500);
+        
+        echo json_encode([
+            "error" => "Internal Server Error",
+            "file" => $e->getFile(),
+            "line" => $e->getLine(),
+            "message" => $e->getMessage()
+        ]);
+    }
+
+    public static function handleError(int $errno, string $errstr, string $errfile, int $errline): bool{
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+
+}
