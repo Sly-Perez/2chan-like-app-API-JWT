a4a6f94c39871048ca9b7acb0021a98368a5f8d5
Sly-Perez <elaipereza@gmail.com>
2025-03-28 10:49:55 -0500

    Gateway folder updated: CommenttImageGateway.php added (function added to get the name of the pictures the commentImages table handles and manages)

---
+++ src/gateway/CommentImageGateway.php
@@ -0,0 +1,35 @@
+<?php
+
+class CommentImageGateway{
+
+    private PDO $dbCon;
+
+    public function __construct(DbConnection $dbConnection){
+        $this->dbCon = $dbConnection->connectDB();
+    }
+
+    public function getByCommentIdAndIndex(string $id, string $index): array | false{
+        $sql = "SELECT
+            commentImageId, name,
+            publishDatetime, userId,
+            commentId
+        FROM commentImages
+        WHERE (commentId=:commentId) AND (state=:state)
+        LIMIT :offset, :rows_count";
+
+        $stmt = $this->dbCon->prepare($sql);
+
+        $stmt->bindValue(":commentId", $id, PDO::PARAM_INT);
+        $stmt->bindValue(":state", 1, PDO::PARAM_INT);
+        $stmt->bindValue(":offset", $index, PDO::PARAM_INT);
+        $stmt->bindValue(":rows_count", 1, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $data;
+    }
+
+    
+}
