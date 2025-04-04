236f991e9a918538d8f93611e7bacd69fa899f36
Sly-Perez <elaipereza@gmail.com>
2025-04-04 13:37:22 -0500

    index.php file updated (Access-Control-Allow-Origin changed to *, to allow everyone to consume the API)

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ spl_autoload_register(function($class){
 });
 
 header("Content-type: application/json");
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
